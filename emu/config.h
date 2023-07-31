1d1ecfa6781eb3d6eddc97a94ba64060876f88b6
Greenchili2 <shannonpope2000@gmail.com>
2023-07-31 02:02:51 -0400

    Remove Visual Studio files

--- emu/config.h
+++ emu/config.h
@@ -4,8 +4,8 @@
 #define SOUND 1
 
 #define INTERPOLATE_SOUND 1
-//#define VOL_ONLY_SOUND 1
-//#define CONSOLE_SOUND 1
+#define VOL_ONLY_SOUND 1
+#define CONSOLE_SOUND 1
 //#define SYNCHRONIZED_SOUND 1
 
 // Define to allow stereo sound. 
